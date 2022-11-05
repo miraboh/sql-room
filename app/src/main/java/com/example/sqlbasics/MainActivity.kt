c861a42f171cf2cdd34a6c06a6936a9693372ffe
miraboh <myrabohuche@gmail.com>
2022-11-05 11:11:48 +0100

    error fixed

--- app/src/main/java/com/example/sqlbasics/MainActivity.kt
+++ app/src/main/java/com/example/sqlbasics/MainActivity.kt
@@ -17,10 +17,12 @@ package com.example.sqlbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
